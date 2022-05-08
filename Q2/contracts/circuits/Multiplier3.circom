43347084d89997602831e630e2b1829af4f66448
Divyam Shah <divyam096@gmail.com>
2022-05-09 01:00:43 +0530

    Assignment Submission

--- Q2/contracts/circuits/Multiplier3.circom
+++ Q2/contracts/circuits/Multiplier3.circom
@@ -1,17 +1,36 @@
 pragma circom 2.0.0;
 
+
+template Multiplier2 () {
+
+   // Declaration of signals.
+   signal input a;
+   signal input b;
+   signal output c;
+
+   // Constraints.
+   c <== a * b;
+}
+
+
 // [assignment] Modify the circuit below to perform a multiplication of three signals
 
-template Multiplier3 () {  
+template Multiplier3 () {
 
-   // Declaration of signals.  
-   signal input a;  
+   // Declaration of signals.
+   signal input a;
    signal input b;
    signal input c;
-   signal output d;  
+   signal output d;
+   component mul1 = Multiplier2();
+   component mul2 = Multiplier2();
 
-   // Constraints.  
-   d <== a * b * c;  
+   // Constraints.
+   mul1.a <== a;
+   mul1.b <== b;
+   mul2.a <== mul1.c;
+   mul2.b <== c;
+   d <== mul2.c;
 }
 
 component main = Multiplier3();
--- Q2/scripts/compile-Multiplier3-plonk.sh
+++ Q2/scripts/compile-Multiplier3-plonk.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-# [assignment] create your own bash script to compile Multipler3.circom using PLONK below
+cd contracts/circuits
+
+mkdir plonk_Multiplier3
+
+if [ -f ./powersOfTau28_hez_final_10.ptau ]; then
+    echo "powersOfTau28_hez_final_10.ptau already exists. Skipping."
+else
+    echo 'Downloading powersOfTau28_hez_final_10.ptau'
+    wget https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_10.ptau
+fi
+
+echo "Compiling Multiplier3.circom..."
+
+# compile circuit
+
+circom Multiplier3.circom --r1cs --wasm --sym -o plonk_Multiplier3
+snarkjs r1cs info plonk_Multiplier3/Multiplier3.r1cs
+
+# Start a new zkey and make a contribution
+
+snarkjs plonk setup plonk_Multiplier3/Multiplier3.r1cs powersOfTau28_hez_final_10.ptau plonk_Multiplier3/circuit_final.zkey
+snarkjs zkey export verificationkey plonk_Multiplier3/circuit_final.zkey plonk_Multiplier3/verification_key.json
+
+# generate solidity contract
+snarkjs zkey export solidityverifier plonk_Multiplier3/circuit_final.zkey ../plonk_Multiplier3Verifier.sol
+
+cd ../..
